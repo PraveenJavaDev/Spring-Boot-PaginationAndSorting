f2fc73efdf8d7c693d996fc9a8622b6e7513358a
PraveenJavaDev <kpraveenjavadev@gmail.com>
2023-10-27 19:02:31 +0530

    firstcommit

---
+++ spring-boot-pagination-sorting/src/main/resources/data.sql
@@ -0,0 +1,22 @@
+INSERT INTO 
+	TBL_EMPLOYEES (first_name, last_name, email) 
+VALUES
+  	('Lokesh', 'Gupta', 'lokesh.guptha@gmail.com'),
+  	('John', 'Doe', 'jhon.doe@email.com'),
+  	('will','smith','will.smith@gmail.com'),
+  	('jhon','cena','jhon.cena@gmail.com'),
+  	('Alice','Jhonson','alice.jhonson@gmail.com'),
+  	('william','lason','william.lason@gmail.com'),
+  	('jackie','chan','jackie.chan@gmail.com'),
+  	('silverstone','stellon','silver.stone@gmail.com'),
+  	('tony','jaa','tonyzaa@gmail.com'),
+  	('praveen','kumar','praveen.k@gmail.com'),
+  	('sathish','kumar','sathish.k@gmail.com'),
+  	('Anil','Kumar','anil.k@gmail.com'),
+  	('pavithra','lokesh','pravithra.l@gmail.com'),
+  	('Aishawarya','rajesh','aishwarys.r@gmail.com'),
+  	('madhuri','kam','madhuri.k@gmail.com'),
+  	('kill','mongar','kill.m@gmail.com'),
+  	('tichalla','black','tichala.b@gmail.com'),
+  	('captain','america','captain.a@gmail.com');
+  	
